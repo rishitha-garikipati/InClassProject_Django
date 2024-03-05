8027d64b3eefd65c5e923e1ea4b9feeaf037a01f
rishitha_garikipati <2200030883@kluniversity.in>
2024-03-05 11:57:24 +0530

    added a movie for the restapi framework using the postman

--- Travel Tourism Management System/restframeworkdemo/moviesapp/urls.py
+++ Travel Tourism Management System/restframeworkdemo/moviesapp/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
-from .views import CreateMovieListView, AllMoviesListView
+from .views import CreateMovieListView, AllMoviesListView, MovieDeleteView, MovieUpdateView
 
 urlpatterns = [
     path('movies/',CreateMovieListView.as_view(),name="movie-list-create"),
-    path('movies/all/',AllMoviesListView.as_view(),name="all-movies-list",)
+    path('movies/all/',AllMoviesListView.as_view(),name="all-moives-list"),
+    path('movies/delete/<int:pk>',MovieDeleteView.as_view(),name="delete-movie"),
+    path('movies/update/<int:pk>',MovieUpdateView.as_view(),name="update-movie"),
 ]
--- Travel Tourism Management System/restframeworkdemo/moviesapp/views.py
+++ Travel Tourism Management System/restframeworkdemo/moviesapp/views.py
@@ -2,12 +2,24 @@ from rest_framework import generics
 from .models import Movie
 from .serializers import MovieSerializer
 from rest_framework.response import Response
-# Create your views here.
 
 class CreateMovieListView(generics.ListCreateAPIView):
-    queryset = Movie.objects.create()
-    serializer_class = MovieSerializer
-
-class AllMoviesListView(generics.RetrieveUpdateDestroyAPIView):
     queryset = Movie.objects.all()
     serializer_class = MovieSerializer
+
+class AllMoviesListView(generics.ListAPIView):
+    queryset = Movie.objects.all()
+    serializer_class = MovieSerializer
+
+class MovieDeleteView(generics.DestroyAPIView):
+    queryset = Movie.objects.all()
+    serializer_class = MovieSerializer
+    def destroy(self, request, *args, **kwargs):
+        instance = self.get_object()
+        instance.delete()
+        return Response(print("Movie Deleted"))
+
+class MovieUpdateView(generics.RetrieveUpdateAPIView):
+    queryset = Movie.objects.all()
+    serializer_class = MovieSerializer
+    partial = True
